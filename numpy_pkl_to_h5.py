bc640e3fb0dbe0a63f793868aaa76a7feaf660ea
Evan Racah <ejracah@gmail.com>
2015-09-28 16:02:40 -0700

    modified pkl to h5 converter and added a util script for getting shape from file

--- numpy_pkl_to_h5.py
+++ numpy_pkl_to_h5.py
@@ -5,10 +5,15 @@ import pickle
 import os
 
 path_to_pkl = sys.argv[1]
-path_to_h5 = sys.argv[2]
-a = pickle.load(open(path_to_pkl))
-output_filename = path_to_pkl.split('/')[1].split('.')[0] + '.h5'
+path_to_h5_input = sys.argv[2]
+ae = pickle.load(open(path_to_pkl))
+orig_data = h5py.File(path_to_h5_input, 'r')
+last_col = np.asarray(orig_data['test'][: , -1])
+ae_l = np.hstack((ae, last_col.reshape((last_col.size,1))))
+output_filename = os.path.basename(path_to_pkl).split('.')[0] + '.h5'
 h5f = h5py.File(output_filename,'w')
-h5f.create_dataset('autoencoded', data=a)
+h5f.create_dataset('autoencoded', data=ae_l)
 h5f.close()
+
+
 
