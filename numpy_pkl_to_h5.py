7d87822dbabc0dc2d5c2b1c557c8fd67c0d1f54d
Evan Racah <ejracah@gmail.com>
2015-10-01 15:44:48 -0700

    Fixed a few errors in split_files and numpy_pkl_to_h5

--- numpy_pkl_to_h5.py
+++ numpy_pkl_to_h5.py
@@ -4,8 +4,9 @@ import numpy as np
 import pickle
 import os
 
-path_to_pkls = sys.argv[1:]
-final = np.array((0, 11))
+save_dir = sys.argv[1]
+path_to_pkls = sys.argv[2:]
+final = np.zeros((0, 11))
 for ind, pkl in enumerate(path_to_pkls):
     ae = pickle.load(open(pkl))
     #labels should come in order
@@ -15,7 +16,7 @@ for ind, pkl in enumerate(path_to_pkls):
     labels = label * np.ones((ae.shape[0],1))
 
     #add labelled column
-    ae = ae.hstack((ae,labels ))
+    ae = np.hstack((ae,labels ))
 
     #stack with previous
     final = np.vstack((final, ae))
@@ -23,7 +24,7 @@ for ind, pkl in enumerate(path_to_pkls):
 
 #puts output in same directory as first pkl input
 output_filename = os.path.basename(path_to_pkls[0]).split('.')[0].split('-')[0] + '.h5'
-h5f = h5py.File(output_filename, 'w')
+h5f = h5py.File(os.path.join(save_dir, output_filename), 'w')
 h5f.create_dataset('autoencoded', data=final)
 h5f.close()
 
--- split_files.py
+++ split_files.py
@@ -13,6 +13,6 @@ examples = h5_data.shape[0]
 ind = examples / classes
 for i in range(classes):
     x = h5_data[i * ind:(i+1) * ind]
-    h5f = h5py.File(basename + '-' + str(i+1) + '.' + extension, 'w')
+    h5f = h5py.File(basename + '-' + str(i+1) + extension, 'w')
     h5f.create_dataset('inputs', data=x)
 
