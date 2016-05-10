ef3e4a60549d4cbf7c1923fe5d16e1701a42a430
Evan Racah <eracah@lbl.gov>
2016-05-09 19:36:09 -0700

    put data_loaders updates in util/data_loaders.py

--- util/data_loaders.py
+++ util/data_loaders.py
@@ -10,132 +10,124 @@ from os.path import join
 from operator import mul
 
 def filter_out_zeros(X,y):
+    
+    #get indices of all rows that don't contain all zeroes
     nonzero_rows = ~np.all(X[:, : ]==0., axis=1)
+    
+    #filter for these nonzero rows
     X = X[nonzero_rows]
     y = y[nonzero_rows]
     return X,y
 
 def get_equal_per_class(X,y, nclass):
-    #get equal number of examples
-    #we assume that y is in one hot encoding form, so the if class is 3 the third element of a row in y is 1.
-    min_class_count = min([X[y[:, cl] == 1.].shape[0] for cl in range(nclass)])
-
-    #a tuple of nlcass arrays where each array is examples from one of the classes and each array is same size
-    X_eq_cl = tuple([X[y[:, cl] == 1.][:min_class_count, :] for cl in range(nclass)])
-    y_eq_cl = tuple([y[y[:, cl] == 1.][:min_class_count, :] for cl in range(nclass)])
-    return X_eq_cl, y_eq_cl, min_class_count
-
-def split_train_test(X,y,nclass, test_prop, seed, eq_class=True):
-    if eq_class:
-        X_eq_cl, y_eq_cl, min_class_count = get_equal_per_class(X,y,nclass)
-        num_ex_per_class = min_class_count
-        num_ex = num_ex_per_class * nclass
-        num_tr_per_class = int((1-test_prop) * num_ex_per_class)
-        num_tr = int( (1-test_prop) * num_ex)
-        i_cl = np.arange(min_class_count)
-        np.random.RandomState(seed).shuffle(i_cl)
-        X_train = np.vstack(tuple([x_cl[i_cl[:num_tr_per_class]] for x_cl in X_eq_cl]))
-        y_train = np.vstack(tuple([y_cl[i_cl[:num_tr_per_class]] for y_cl in y_eq_cl]))
-        X_test = np.vstack(tuple([x_cl[i_cl[num_tr_per_class:]] for x_cl in X_eq_cl]))
-        y_test = np.vstack(tuple([y_cl[i_cl[num_tr_per_class:]] for y_cl in y_eq_cl]))
-        
-    else:
-        num_ex = X.shape[0]
-        i = np.arange(X.shape[0])
-        np.random.RandomState(seed).shuffle(i)
-        X_test = X[i[:test_prop * num_ex]]
-        X_train  = X[i[test_prop * num_ex:]]
-
-        y_train =  y[i[test_prop * num_ex:]]
-        y_test =  y[i[:test_prop * num_ex]]
-
-        num_tr = X_train.shape[0]
-
-    return X_train, y_train, X_test, y_test, num_tr
-
-def split_train_val(X_train, y_train, seed, val_prop, num_tr):
-    ix = np.arange(X_train.shape[0])
-    np.random.RandomState(seed).shuffle(ix)
-    X_train = X_train[ix]
-    y_train = y_train[ix]
-    num_val = val_prop * num_tr
-    X_val = X_train[:num_val]
-    y_val = y_train[:num_val]
-    X_train = X_train[num_val:]
-    y_train = y_train[num_val:]
-    return X_train, y_train, X_val, y_val
+#     #get the number of rows in X that correspond to a y for each given class, then take the class that 
+#     #corresponds to the fewest rows in X
+#     min_classes = [X[y == float(cl)].shape[0] for cl in range(nclass)]
+#     print min_classes
+#     min_class_count = min(min_classes)
+#     #get min_class_count rows of X for each corresponding class
+#     print y.shape
+#     X = np.vstack(tuple([X[y == float(cl)][:min_class_count, :] for cl in range(nclass)]))
+#     y = np.vstack(tuple([y[y == float(cl)][:min_class_count] for cl in range(nclass)]))
+#     print y.shape
+    
+    return X,y
 
 
+def make_divisible_by_batch_size(X,y,batch_size):
+    if batch_size:
+        num = batch_size * (X.shape[0] / batch_size)
+        X = X[:num]
+        y = y[:num]
+    return X, y
 
-def load_dayabay_conv(path,clev_preproc=False,filter_size=3, just_test=False, test_prop=0.2, validation=True, val_prop=0.2, seed=3, get_y=True, eq_class=True, tr_size=None):
+def split_dataset_in_2(X, y, proportion, seed=3, batch_size=None):
+    
+    #make indices for every row
+    num_ex = X.shape[0]
+    i = np.arange(X.shape[0])
+    
+    #shuffle the indices
+    #no shuffle. file is already shuffled 
+    #np.random.RandomState(seed).shuffle(i)
+    
+    #allocate proportion*100 percent of the indices for first dataset
+    #and the rest for second
+    first_inds = i[:proportion * num_ex]
+    second_inds = i[proportion * num_ex:]
+    
+    #indexes the correct rows from X and y and make sure the number of examples is divisible batch size so no weird erros
+    #with X and y
+    X_first, y_first = make_divisible_by_batch_size(X[first_inds], y[first_inds].reshape(first_inds.shape[0]), batch_size)
+    X_second, y_second = make_divisible_by_batch_size(X[second_inds], y[second_inds].reshape(second_inds.shape[0]), batch_size)
+    
+    return X_first, y_first, X_second, y_second
+
+
+def do_geom_preproc(X):
+    X = X.reshape(X.shape[0],8,24)
+    #pad the right w ith the first filter_size-1 columns from the left
+    X_p = np.lib.pad(X, ((0, 0), (0, 0), (0, filter_size - 1)), 'wrap')
+
+    #add another 8,24 array that is shifted by 12
+    X_s_p = np.lib.pad(X_p, ((0,0), (0,0), (0, 11)), 'wrap')[:,:,11:]
+    X_t = np.hstack((X_p, X_s_p))
+
+    #flatten
+    X_t = X_t.reshape(X_t.shape[0], reduce(mul, X_t.shape[1:]))
+    return X_t
+
+def mean_subtract_unit_var(X):
+    X -= np.mean(X)
+    X /= np.std(X)
+    return X
+    
+
+def load_dayabay_conv(path,geom_preproc=False,filter_size=3, just_test=False, test_prop=0.2, validation=True, val_prop=0.2, seed=3, get_y=True, eq_class=True, tr_size=None, batch_size=None):
+    '''Expects hdf5 with
+            dataset inputs: (n,192+), where first 192 columns are the charge deposits
+            dataset label_one_hot: (n,5), where each row is a one hot encoding of the class (1-5)'''
     nclass=5
+    
     h5_dataset = h5py.File(path)
+    
     if not tr_size:
         tr_size = h5_dataset['inputs'].shape[0]
         
     X = np.asarray(h5_dataset['inputs'][:tr_size,:192]).astype('float64')
     if get_y:
-        y = np.asarray(h5_dataset['label_one_hot'][:tr_size, :]).astype('float64')
-    else:
-        y = np.ones((X.shape[0],5))
+        y = np.asarray(h5_dataset['targets'][:tr_size]).astype('float64')
 
+    y = np.argmax(y,axis=1).reshape(y.shape[0],)
+    
 
-    # X -= np.mean(X)
-    # X /= np.std(X)
+    #just to make sure there are no events with all zeroes
     X, y = filter_out_zeros(X,y)
-    if clev_preproc:
-        X = X.reshape(X.shape[0],8,24)
-        #pad the right w ith the first filter_size-1 columns from the left
-        X_p = np.lib.pad(X, ((0, 0), (0, 0), (0, filter_size - 1)), 'wrap')
+    #X = mean_subtract_unit_var(X)
+    
+    #do geometry preprocessing
+    if geom_preproc:
+        X = do_geom_preproc(X)
+    
 
-        #add another 8,24 array that is shifted by 12
-        X_s_p = np.lib.pad(X_p, ((0,0), (0,0), (0, 11)), 'wrap')[:,:,11:]
-        X_t = np.hstack((X_p, X_s_p))
-
-        #flatten
-        X_t = X_t.reshape(X_t.shape[0], reduce(mul, X_t.shape[1:]))
-    else:
-        X_t = X
+    #make an equal number of examples for each class
+    if eq_class:
+        X, y = get_equal_per_class(X, y, nclass)
 
 
 
-    if not just_test:
-        X_train, y_train, X_test, y_test, num_tr = split_train_test(X_t, y, nclass, test_prop, seed,eq_class)
+    #reshape into 8,24
+    
+    X = X.reshape(X.shape[0], 1, 8, 24)
+    
+    #split into train and test
+    X_test, y_test, X_train, y_train = split_dataset_in_2(X, y, test_prop, seed, batch_size=batch_size)
 
 
-
+    #split train up again into train and val
     if validation:
-        X_train, y_train, X_val, y_val = \
-                split_train_val(X_train, y_train, seed, val_prop, num_tr)
-
-    # tr_mean = np.mean(X_train)
-    # tr_std = np.std(X_train)
-    # X_train -= tr_mean
-    # X_test -= tr_mean
-    # X_train /= tr_std
-    # X_test /= tr_std
-    #
-    # if validation:
-    #         X_val -= tr_mean
-    #         X_val /= tr_std
-
+         X_val, y_val, X_train, y_train = \
+                split_dataset_in_2(X_train, y_train, val_prop, seed, batch_size=batch_size)
 
 
     return (X_train, y_train), (X_val,y_val), (X_test, y_test), nclass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
