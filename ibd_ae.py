84bc1354215eaacfcd1c7d10832d80d7b7eb9b90
Sam <kohn@berkeley.edu>
2016-06-08 16:09:03 -0700

    Update ibd_ae.py to use new IBDPairConvAe
    It's much simpler!

--- ibd_ae.py
+++ ibd_ae.py
@@ -3,8 +3,6 @@ import numpy as np
 import os
 import pickle
 import sys
-from neon.util.argparser import NeonArgparser
-
 import h5py
 import matplotlib
 from sklearn.manifold import TSNE
@@ -12,14 +10,12 @@ import numpy as np
 matplotlib.use('agg')
 from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
-from conv_ae import ConvAe
 from vis.viz import Viz
 from util.helper_fxns import adjust_train_val_test_sizes
 from util.data_loaders import load_ibd_pairs
-
-
 from util.data_loaders import load_dayabay_conv
-
+from LasagneConv import IBDPairConvAe
+import logging
 
 
 
@@ -32,80 +28,25 @@ from util.data_loaders import load_dayabay_conv
 
 # In[118]:
 
-#neon makes you set things up with command line args, so here we set all the defaults
-def setup_parser():
-        # parse the command line arguments
-        parser = NeonArgparser(__doc__)
-        #main data file
-        parser.add_argument('--h5file')
-        
-        #whether you are doing test or just train and val
-        parser.add_argument('--test')
-        
-        parser.add_argument('--learn_rate')
-        
-        #hidden layer dimension for bottle neck of autoencoder
-        parser.add_argument('--bneck_width')
-        
-        #how many iterations of tsne
-        parser.add_argument('--max_tsne_iter')
-        parser.set_defaults(batch_size=128,
-                            h5file='/project/projectdirs/dasrepo/single_20000.h5',
-                            
-                            #take a snapshot every n epochs where n is value fo serialize
-                            serialize=2, 
-                            
-                            #how many epochs
-                            epochs=100, 
-                            learn_rate=0.0001, 
-                            
-                            #where to save serialized weights for a model snapshot 
-                            model_file=False,
-                            
-                            #how frequently to log validation data
-                            eval_freq=1, 
-                            test=False, 
-                            
-                            #where to save any results
-                            save_path='./results/model_files/conv-ae2',
-                            
-                            #these flags are for doing the preprocessing geometrical tricks 
-                            wrap_pad_trick=False, 
-                            cylinder_local_trick=False, 
-                            
-                            bneck_width=10, 
-                            max_tsne_iter=500)
-
-        args = parser.parse_args()
-        
-        #convert strings to floats and ints
-        args.learn_rate = float(args.learn_rate)
-        args.max_tsne_iter = int(args.max_tsne_iter)
-        
-        return args
-
-
-
-
 if __name__ == "__main__":
-    
-    #sys.argv = sys.argv[5:] # only for iPython to skip all the ipython command line arguments
-    args = setup_parser()
-    #load data from hdf5, preprocess and split into train and test
-    preprocess = True
-    train, val, test = load_ibd_pairs(args.h5file, preprocess=preprocess)
-
-    train, _, val, _, test, _  = adjust_train_val_test_sizes(args.batch_size,
-        train, train, val, val, test, test)
 
     #class for networks architecture
-    cae = ConvAe(args, nchannels=4)
+    cae = IBDPairConvAe()
+    
+    h5file = "../dayabay-data-conversion/extract_ibd/ibd_yasu_0_9999.h5"
+    #load data from hdf5, preprocess and split into train and test
+    preprocess = True
+    train, val, test = load_ibd_pairs(h5file, preprocess=preprocess)
+
+    train, _, val, _, test, _  = adjust_train_val_test_sizes(cae.minibatch_size,
+        train, train, val, val, test, test)
 
     #uses scikit-learn interface (so this trains on X_train)
     cae.fit(train)
 
     #extract the hidden layer outputs when running x_val thru autoencoder
-    feat = cae.extract(val)
+    feat = cae.extract_layer(val, 'bottleneck')[:, :, 0, 0]
+    logging.debug('feat.shape = %s', str(feat.shape))
     gr_truth = np.ones(val.shape[0])
 
     v = Viz(gr_truth)
