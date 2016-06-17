f546acba38a5c4ce1628fe7521b1d4b70c124512
eracah <ejracah@gmail.com>
2016-06-17 14:14:41 -0700

    Cleaned up some data loading code and moved it to data_loaders module.

--- ibd_ae.py
+++ ibd_ae.py
@@ -11,10 +11,7 @@ matplotlib.use('agg')
 from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
 from vis.viz import Viz
-from util.helper_fxns import adjust_train_val_test_sizes
-from util.helper_fxns import center, scale
-from util.data_loaders import load_ibd_pairs
-from util.data_loaders import load_dayabay_conv
+from util.data_loaders import load_ibd_pairs, get_ibd_data
 from networks.LasagneConv import IBDPairConvAe
 import logging
 
@@ -35,26 +32,7 @@ if __name__ == "__main__":
     #class for networks architecture
     cae = IBDPairConvAe(epochs=epochs)
     
-    #load data from hdf5, preprocess and split into train and test
-    train = np.zeros((20000, 4, 8, 24))
-    val = np.zeros((10000, 4, 8, 24))
-    test = np.zeros((10000, 4, 8, 24))
-    h5files = []
-    for i in range(4):
-        name = "/global/homes/s/skohn/ml/dayabay-data-conversion/extract_ibd/ibd_yasu_%d_%d.h5"
-        h5file = name % (i*10000, (i+1)*10000-1)
-        (train[i*5000:(i+1)*5000], val[i*2500:(i+1)*2500],
-            test[i*2500:(i+1)*2500]) = load_ibd_pairs(h5file)
-
-    center(train)
-    center(val)
-    center(test)
-    scale(train, 1)
-    scale(val, 1)
-    scale(test, 1)
-
-    train, _, val, _, test, _  = adjust_train_val_test_sizes(cae.minibatch_size,
-        train, train, val, val, test, test)
+    train, val, test = get_ibd_data()
 
     #uses scikit-learn interface (so this trains on X_train)
     cae.fit(train)
@@ -72,4 +50,3 @@ if __name__ == "__main__":
 
     #plot the 2D-projection of the features
     v.plot_features(x_pc,save=True)
-
