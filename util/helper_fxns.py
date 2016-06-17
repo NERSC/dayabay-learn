ba45e489de491be7bf9dc5f393356ab963e5ace1
eracah <ejracah@gmail.com>
2016-06-17 14:13:56 -0700

    got rid of making in place optional and added a scale between min max option aka normalization, which can be an alternative to standardization (having a certain stdv). Also got rid of extra code

--- util/helper_fxns.py
+++ util/helper_fxns.py
@@ -1,12 +1,9 @@
-import matplotlib
-matplotlib.use('agg')
-from matplotlib import pyplot as plt
 import numpy as np
 import os
-import h5py
-from collections import deque
 
-def center(data, in_place=True):
+
+#getting rid of the in place option (just complicates things. When would we not do this in place?)
+def center(data):
     '''Subtract the mean over all samples and pixels, independent by channel.
 
     If in_place, update the given data array and return the array of means.
@@ -14,14 +11,11 @@ def center(data, in_place=True):
 
     Expects data of the form [batch, channel, height, width].'''
     means = data.mean(axis=(0, 2, 3), keepdims=True)
-    if in_place:
-        data -= means
-        return means.flatten()
-    else:
-        result = data - means
-        return means.flatten(), result
+    #due to numpy objects passed by reference (almost forgot about this)
+    data -= means
+    return means.flatten()
 
-def scale(data, std, in_place=True):
+def scale(data, std=None, mode="standardize"):
     '''Scale the data to the given std over all samples and pixels, independent
     by channel.
 
@@ -29,138 +23,54 @@ def scale(data, std, in_place=True):
     Else, return (array of stds, new array).
 
     Expects data of the form [batch, channel, height, width].'''
-    stds = data.std(axis=(0, 2, 3), keepdims=True)
-    if in_place:
+    
+    if mode == "standardize":
+        stds = data.std(axis=(0, 2, 3), keepdims=True)
         data /= stds/std
         return stds.flatten()
+    elif mode == "normalize":
+        scale_min_max(data)
     else:
-        result = data * (std / stds)
-        return stds.flatten(), result
-
-def save_orig_data(h5fin,X_train, y_train, X_val,y_val, X_test, y_test):
-    h5fin.create_dataset('raw/train/x', data=X_train)
-    h5fin.create_dataset('raw/train/y', data=y_train)
-    h5fin.create_dataset('raw/test/x', data=X_test)
-    h5fin.create_dataset('raw/test/y', data=y_test)
-    h5fin.create_dataset('raw/val/x', data=X_val)
-    h5fin.create_dataset('raw/val/y', data=y_val)
-
-
-def plot_train_val_learning_curve(final_h5_file):
-    h5fin = h5py.File(final_h5_file)
-    plt.clf()
-    v_loss = np.asarray(h5fin['valid_loss/cost/loss'])
-    t_loss = np.asarray(h5fin['train_loss/cost/loss'])
-    plt.plot(range(v_loss.shape[0]), v_loss)
-    plt.plot(range(t_loss.shape[0]), t_loss)
-    plt.xlabel('epochs')
-    plt.ylabel('sum squared error')
-    plt.legend(['val_loss', 'tr_loss'])
-    plt.savefig(final_h5_file + '.pdf')
-
-
-def adjust_train_val_test_sizes(batch_size, X_train, y_train, X_val, y_val, X_test, y_test ):
-    #make sure size of validation data a multiple of batch size (otherwise tough to match labels)
-
-    train_end = batch_size * (X_train.shape[0] / batch_size)
-    X_train = X_train[:train_end]
-    y_train = y_train[:train_end]
-
-    val_end = batch_size * (X_val.shape[0] / batch_size)
-    X_val = X_val[:val_end]
-    y_val = y_val[:val_end]
-
-    #make sure size of test data a multiple of batch size
-    test_end = batch_size * (X_test.shape[0] / batch_size)
-    X_test = X_test[:test_end]
-    y_test = y_test[:test_end]
-
-    return X_train, y_train, X_val, y_val, X_test, y_test
-
-
-def save_middle_layer_output(dataset, dset, model, bneck_width):
-    arr = get_middle_layer_output(dataset,model, bneck_width)
-    dset[:] = arr[:]
-#     ix = 0
-#     for (x, t) in dataset:
-#         for l in model.layers.layers:
-#                 #forward propagate the data through the deepnet
-#                 x = l.fprop(x)
-#                 if l.name == 'middleLayer' or l.name == 'middleActivationLayer': #trying to get middle layer here
-#                     ae_x = x.asnumpyarray()
-
-#                     #ae_x is transposed from what we expect, so its of size (bneck_width, batch_size)
-#                     layer_width, batch_size = ae_x.shape
-#                     assert layer_width == bneck_width
-
-#                     #data iterator will do wrap around, so in the last batch's last several items
-#                     # will be the first several from the first batch
-#                     if ix + batch_size > dataset.ndata:
-#                         dset[ix:dataset.ndata] = ae_x.T[:dataset.ndata - ix]
-#                         # val_arr[ix:dataset.ndata, :] = ae_x.T[:dataset.ndata - ix]
-
-#                     else:
-#                         dset[ix:ix+batch_size] = ae_x.T
-#                         # val_arr[ix:ix+batch_size, :] = ae_x.T
-#                         ix += batch_size
-#                     break;
-def get_middle_layer_output(dataset,model, bneck_width):
-    # iterates through dataset and then thru network to extract the features in the middle layer
-    features = np.zeros((dataset.ndata, bneck_width))
-    ix = 0
-    for (x, t) in dataset:
-        for l in model.layers.layers:
-                #forward propagate the data through the deepnet
-                x = l.fprop(x)
-                if l.name == 'middleLayer': #trying to get middle layer here
-                    print ae_x
-                    ae_x = x.asnumpyarray()
-
-                    #ae_x is transposed from what we expect, so its of size (bneck_width, batch_size)
-                    layer_width, batch_size = ae_x.shape
-                    assert layer_width == bneck_width
-
-                    #data iterator will do wrap around, so in the last batch's last several items
-                    # will be the first several from the first batch
-                    if ix + batch_size > dataset.ndata:
-                        features[ix:dataset.ndata] = ae_x.T[:dataset.ndata - ix]
-                        # val_arr[ix:dataset.ndata, :] = ae_x.T[:dataset.ndata - ix]
-
-                    else:
-                        features[ix:ix+batch_size] = ae_x.T
-                        # val_arr[ix:ix+batch_size, :] = ae_x.T
-                        ix += batch_size
-                    break;
-    return features
+        raise NotImplementedError
     
+def scale_min_max(data, min_=-1, max_=1):
+    '''scales data to be between min and max in place'''
+    mins = data.min(axis=(0, 2, 3), keepdims=True)
+    maxes = data.max(axis=(0, 2, 3), keepdims=True)
+
+    #data = 2 * ((data - mins) / (maxes - mins)) - 1
+    #in place
+    data -= mins
+    data /= (maxes-mins)
+    data *= 2
+    data -= 1
+    
+def filter_out_zeros(X,y):
+    
+    #get indices of all rows that don't contain all zeroes
+    nonzero_rows = ~np.all(X[:, : ]==0., axis=1)
+    
+    #filter for these nonzero rows
+    X = X[nonzero_rows]
+    y = y[nonzero_rows]
+    return X,y
+
+def get_equal_per_class(X,y, nclass):
+#     #get the number of rows in X that correspond to a y for each given class, then take the class that 
+#     #corresponds to the fewest rows in X
+#     min_classes = [X[y == float(cl)].shape[0] for cl in range(nclass)]
+#     print min_classes
+#     min_class_count = min(min_classes)
+#     #get min_class_count rows of X for each corresponding class
+#     print y.shape
+#     X = np.vstack(tuple([X[y == float(cl)][:min_class_count, :] for cl in range(nclass)]))
+#     y = np.vstack(tuple([y[y == float(cl)][:min_class_count] for cl in range(nclass)]))
+#     print y.shape
+    
+    return X,y
 
 
-def get_eq_classes_of(y, points_per_class, nclass):
-    y_ind = np.arange(y.shape[0])
-    indices = np.asarray([y_ind[y[:, cl] == 1.][:points_per_class] for cl in range(nclass)]).reshape(nclass * points_per_class)
 
-    return indices
 
-def create_h5_file(final_dir, *args):
-    final_h5_file = os.path.join(final_dir, 'final_results_tr_size' + '_'.join(map(lambda x: str(x), args)) + '.h5')
-    h5fin = h5py.File(final_h5_file, 'w')
-    return h5fin, final_h5_file
-
-def stop_func(s,v):
-    if s is None:
-        return (deque([v],maxlen=5), False)
-    s.append(v)
-    return (s, v == np.mean(s))
-
-def save_image(vec, name, y_offset=0.3, x_offset=0.69, font_size=5):
-    plt.clf()
-    im = vec.reshape(8, 24)
-    ax = plt.imshow(im, interpolation='none')
-    left, right, bottom, top = ax.get_extent()
-    plt.colorbar(orientation="horizontal")
-    for x in range(im.shape[0]):
-        for y in range(im.shape[1]):
-            plt.text(left + y_offset + y, top + x_offset + x, '%.2f' % (im[x, y]), fontsize=font_size)
-    plt.savefig(name + '.jpg')
 
 
