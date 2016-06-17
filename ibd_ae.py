b7b0bdde7d66b82c5c5aef2309331fb258e46b33
Sam <kohn@berkeley.edu>
2016-06-17 13:00:27 -0700

    Add logging messages

--- ibd_ae.py
+++ ibd_ae.py
@@ -15,6 +15,7 @@ from util.data_loaders import load_ibd_pairs, get_ibd_data
 from networks.LasagneConv import IBDPairConvAe
 import argparse
 import logging
+logging.basicConfig(format='%(levelname)s:\t%(message)s')
 
 
 
@@ -40,18 +41,23 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     #class for networks architecture
+    logging.info('Constructing untrained ConvNet')
     cae = IBDPairConvAe(bottleneck_width=args.bottleneck_width,
         epochs=args.epochs)
+    logging.info('Preprocessing data files')
     train, val, test = get_ibd_data(tot_num_pairs=10000)
 
     #uses scikit-learn interface (so this trains on X_train)
+    logging.info('Training network')
     cae.fit(train)
 
     #extract the hidden layer outputs when running x_val thru autoencoder
+    logging.info('Extracting bottleneck layer')
     feat = cae.extract_layer(val, 'bottleneck')[:, :, 0, 0]
     logging.debug('feat.shape = %s', str(feat.shape))
     gr_truth = np.ones(val.shape[0])
 
+    logging.info('Constructing visualization')
     v = Viz(gr_truth)
 
     # take first two principal components of features, so we can plot easily
